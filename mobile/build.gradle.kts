6e143e743c2072e2807fb1558d5dc517cca402d1
AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>
2025-11-20 00:20:03 +0200

    bump app version to 0.0.2
    More Code Refactoring and Improvements
    
    Change-Id: I6c8ffc8dcff557656d1b201e515fe12b035c1bc5
    Signed-off-by: AbdAlMoniem AlHifnawy <hifnawy_moniem@hotmail.com>

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -84,8 +84,8 @@ android {
 
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
